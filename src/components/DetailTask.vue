0f987dd7ce6f11f0625be92ac61840298eeb8d52
Chedly <chedly.marzougui@gmail.com>
2023-09-06 07:00:20 +0100

    create detailTask component

---
+++ src/components/DetailTask.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useTask } from "../composable/task";
+
+const { selectedTask } = useTask();
+</script>
+<template>
+  <h1>deyail task</h1>
+  <h2>{{ selectedTask }}</h2>
+</template>
