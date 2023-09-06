723eeba9d2c5ce75c3439c2a9180ebf144e2da4e
Chedly <chedly.marzougui@gmail.com>
2023-09-06 07:00:40 +0100

    create listTask component

---
+++ src/components/ListTask.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useTask } from "../composable/task";
+
+const { taskList, selectTask, deleteTask } = useTask();
+</script>
+
+<template>
+  <h2 class="h-2 m-4">Task List</h2>
+  <div v-for="task in taskList" :key="task">
+    <span @click="selectTask(task)">{{ task }}</span>
+    <button @click="deleteTask(task)">delete</button>
+  </div>
+</template>
