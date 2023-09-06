630ed4e7f3a8eb64a3e2d64eeb2abda0ce3d4f5b
Chedly <chedly.marzougui@gmail.com>
2023-09-06 06:59:43 +0100

    create AddTask component

---
+++ src/components/AddTask.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useTask } from "../composable/task";
+const { addTask } = useTask();
+
+const newTask = ref<string>("");
+</script>
+
+<template>
+  <h3>Add Task</h3>
+  <input type="text" v-model="newTask" />
+  <button @click="() => addTask(newTask)">Add Task</button>
+</template>
