66090d2a5ca459c0a4dcbb0420ce98bc0a3585cd
Chedly <chedly.marzougui@gmail.com>
2023-09-09 12:21:04 +0100

    Create post compsable

--- src/composable/post.ts
+++ src/composable/post.ts
@@ -1,16 +1,38 @@
-import axios from axios
+import { ref } from "vue";
+import axios from "axios";
 
-export function usePost(){
-  const uri = "https://jsonplaceholder.typicode.com/posts"
-  
-  const fetchPostList = async()=>{
+const postList = ref<Array<any>>([]);
+const selectedPost = ref<any>(null);
+
+export function usePost() {
+  const uri = "https://jsonplaceholder.typicode.com/posts/";
+
+  const fetchPostList = async () => {
     try {
-      const data = await axios.get(uri)
-      console.log(data);
-      
-      
+      const { data } = await axios.get(uri);
+      postList.value = { ...data };
     } catch (error) {
-      trow error
+      throw error;
     }
-  }
+  };
+
+  const fetchPostById = async (id: string) => {
+    try {
+      const { data } = await axios.get(`${uri}${id}`);
+      selectedPost.value = data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const deletePost = async (postId: string) => {
+    try {
+      await axios.delete(`${uri}${postId}`);
+      fetchPostList();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  return { postList, selectedPost, fetchPostList, fetchPostById, deletePost };
 }
