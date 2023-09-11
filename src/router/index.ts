fca66eb4a563c9358d25b79cca2eff69c2a9bf41
Chedly <chedly.marzougui@gmail.com>
2023-09-11 19:21:30 +0100

    create router instence & configure routes

---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+import ListPost from "../components/ListPost.vue";
+import AddPost from "../components/AddPost.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Post List",
+    component: ListPost,
+  },
+  {
+    path: "/add",
+    name: "Add Post",
+    component: AddPost,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
